cd21171111de00397e9d3c43d2d152ffc4d42a9c
EdsonFagner <edson.fagner.souto@gmail.com>
2021-12-07 17:43:25 -0300

    alteracoes

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,14 @@
 //Criando variáveis para armazenar posições do vetor curso
-let curso1 = cursos[0];
-let curso2 = cursos[1];
-let curso3 = cursos[2];
+//let curso1 = cursos[0];
+//let curso2 = cursos[1];
+//let curso3 = cursos[2];
 
 //Definindo uma variavel constante para armazenar comando de seleção unica.
-const c = (el)=>document.querySelector(el);
+const c = (el) => document.querySelector(el);
 
 
 
-verCursos = (idIni, tituloIni, descricaoIni, nomeIni){
+/*verCursos = (idIni, tituloIni, descricaoIni, nomeIni){
 
     let containerVerCursos = document.querySelector('#container__verCursos');
 
@@ -32,9 +32,9 @@ verCursos = (idIni, tituloIni, descricaoIni, nomeIni){
     div1.appendChild(div1Title);
     div1.appendChild(div1Desc);
     div1FPiece.appendChild(div1PNome);
-}
+}*/
 
-verPosts = (idIniPost, tituloIniPost, descricaoIniPost, nomeIniPost){
+/*verPosts = (idIniPost, tituloIniPost, descricaoIniPost, nomeIniPost){
     let containerVerCursos = document.querySelector('#container__verPosts');
 
     let div2 = document.createElement('div');
@@ -57,7 +57,7 @@ verPosts = (idIniPost, tituloIniPost, descricaoIniPost, nomeIniPost){
     div2.appendChild(div2Title);
     div2.appendChild(div2Desc);
     div2FPiece.appendChild(div2PNome);
-}
+}*/
 
 
 
@@ -67,7 +67,7 @@ verPosts = (idIniPost, tituloIniPost, descricaoIniPost, nomeIniPost){
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Função para ao clicar modificar os itens da página modificando as opções de display para INICIO
-c('#pagina--inicial').addEventListener('click', ()=>{
+c('#pagina--inicial').addEventListener('click', () => {
     c('#container__pagInicial').style.display = "flex";
     c('#container__verCursos').style.display = "none";
     c('#container__verPost').style.display = "none";
@@ -75,26 +75,47 @@ c('#pagina--inicial').addEventListener('click', ()=>{
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Função para ao clicar modificar os itens da página modificando as opções de display para INICIO
-c('#ver--cursos').addEventListener('click', ()=>{
-    c('#container__pagInicial').style.display = "none";
-    c('#container__verCursos').style.display = "flex";
-    c('#container__verPost').style.display = "none";
-    verCursos(curso1.id, curso1.titulo, curso1.descricao, curso1.nomeProfessor);
-    verCursos(curso2.id, curso2.titulo, curso2.descricao, curso2.nomeProfessor);
-    verCursos(curso3.id, curso3.titulo, curso3.descricao, curso3.nomeProfessor);
-    
-    for(let i = 0; i < cursos.length; i++){
-        verCursos(cursos[i].id, cursos[i].titulo, cursos[i].descricao, cursos[i].nomeProfessor)
-    }
+c('#ver--cursos').addEventListener('click', () => {
+    document.getElementById('container__inicial').style.display = "none";
+    document.getElementById('container__verCursos').style.display = "flex";
+    document.getElementById('container__verPost').style.display = "none"
+    fetch('https://test.godigibee.io/pipeline/gama/v1/gama-digibee?route=cursos', { method: 'GET', headers: {apikey: '3OUYJz5DrI7w5SMMJL2EVmO93JREo6Nm' } }).then( (cursos) => {
+        /*for(let i = 0; i < cursos.length; i++){
+            verCursos(cursos[i].id, cursos[i].titulo, cursos[i].descricao, cursos[i].nomeProfessor)
+        }*/
+        console.log(cursos)
+        return cursos.json()
+
+    })
+    .then((cursos)=>{
+        console.log(cursos)
+    })
+    .catch(error => {
+        console.log(error);
+    })
+
 });
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Função para ao clicar modificar os itens da página modificando as opções de display para INICIO
-c('#ver--post').addEventListener('click', ()=>{
-    c('#container__pagInicial').style.display = "none";
-    c('#container__verCursos').style.display = "none";
-    c('#container__verPost').style.display = "flex";
-    for(let x = 0; x < posts.length; i++){
+c('#ver--post').addEventListener('click', () => {
+    document.getElementById('container__inicial').style.display = "none";
+    document.getElementById('container__verCursos').style.display = "none";
+    document.getElementById('container__verPost').style.display = "flex";
 
-    }
+    fetch('https://test.godigibee.io/pipeline/gama/v1/gama-digibee?route=posts', { method: 'GET', headers: { apiKey: '3OUYJz5DrI7w5SMMJL2EVmO93JREo6Nm',  } }).then(function (cursos) {
+        /*for(let i = 0; i < cursos.length; i++){
+            verCursos(cursos[i].id, cursos[i].titulo, cursos[i].descricao, cursos[i].nomeProfessor)
+        }*/
+        console.log(cursos.json.toString())
+    }).catch(error => {
+        console.log(error);
+    })
+
+    /*for(let x = 0; x < posts.length; i++){
+
+    }*/
 });
+
+c('#container__verCursos').style.display = "none";
+c('#container__verPost').style.display = "none";
