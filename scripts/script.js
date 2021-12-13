ae52d023cd4178097864681f42e9338b00a2e940
NobreJanio <janionobrega@hotmail.com>
2021-12-13 16:03:39 -0300

    criei uma galeria

--- scripts/script.js
+++ scripts/script.js
@@ -170,5 +170,40 @@ function exit() {
 }
 
 
+////////////////////////////////////////////////////
+
+const img = document.getElementById('carousel');
+const rightBtn = document.getElementById('right-btn');
+const leftBtn = document.getElementById('left-btn');
+
+// Images are from unsplash
+let pictures = ['../img/a.jpg', '../img/b.jpg', '../img/c.jpg','../img/d.jpg','../img/e.jpg','../img/f.jpg'];
+
+img.src = pictures[0];
+let position = 0;
+
+const moveRight = () => {
+    if (position >= pictures.length - 1) {
+        position = 0
+        img.src = pictures[position];
+        return;
+    }
+    img.src = pictures[position + 1];
+    position++;
+}
+
+const moveLeft = () => {
+    if (position < 1) {
+        position = pictures.length - 1;
+        img.src = pictures[position];
+        return;
+    }
+    img.src = pictures[position - 1];
+    position--;
+}
+
+rightBtn.addEventListener("click", moveRight);
+leftBtn.addEventListener("click", moveLeft);
+
 
 
